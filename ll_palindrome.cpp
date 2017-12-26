34bb99cd03b8b6fea345c2e37a889a0b2dfb2826
Ilya A. Kriveshko <iillyyaa@gmail.com>
2017-12-26 12:11:42 -0500

    ll_palindrome: add main and comments

--- ll_palindrome.cpp
+++ ll_palindrome.cpp
@@ -1,29 +1,32 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ * Determine if a given list is a palindrome.
+ * This solution is linear time, constant memory. It temporarily modifies
+ * the list, but returns it to the original state.
+ * It is not robust to loops in the list.
  */
-int isPalindrome(ListNode* A) {
+int isPalindrome(ListNode * const list) {
+    // Obtain the length of the list
     int len = 0;
-    ListNode *cur = A;
-    while(cur) {
-        ++len;
+    for(ListNode *cur = list; cur; ++len) {
         cur = cur->next;
     }
-    
+
+    // Trivial degenerate cases
     if (len <= 1) {
         return 1;
     }
-    if (len == 2) {
-        return A->val == A->next->val;
-    }
-    
-    // Reverse half the list
+
+    // Reverse the first half of the list
     ListNode *prev = nullptr;
-    cur = A;
+    ListNode *cur = list;
     int numRev = 0;
     while (numRev < len/2) {
         ListNode *next = cur->next;
@@ -32,7 +35,7 @@ int isPalindrome(ListNode* A) {
         cur = next;
         ++numRev;
     }
-    
+
     // Now cur points to the second half
     // And prev points to the first half, reversed
     ListNode *fwd = cur;
@@ -41,19 +44,52 @@ int isPalindrome(ListNode* A) {
         // Odd number of nodes, so skip the middle element
         fwd = fwd->next;
     }
+
+    // Re-reverse the first half, while at the same time
+    // traversing the second half and comparing the values
     int palindrome = 1;
-    ListNode *bwdPrev = cur;
+    ListNode *bwdPrev = cur; // starting point for re-linking the two halfs
     while(fwd && bwd) {
         if (fwd->val != bwd->val) {
             palindrome = 0;
         }
-        
+
         ListNode *next = bwd->next;
         bwd->next = bwdPrev;
         bwdPrev = bwd;
         bwd = next;
         fwd = fwd->next;
     }
-    
+
     return palindrome;
+}
+
+int main(int argc, char const * const * argv) {
+    ListNode *head = nullptr;
+    ListNode *tail = nullptr;
+
+    for(int c=1; c<argc; ++c) {
+        ListNode *nn = new ListNode(atoi(argv[c]));
+        if (tail) {
+            tail->next = nn;
+        } else {
+            head = nn;
+        }
+        tail = nn;
+    }
+
+    std::cerr << "Input:\n";
+    for(ListNode *l=head; l; l=l->next) {
+        std::cerr << l->val << "\n";
+    }
+
+    int palindrome = isPalindrome(head);
+    std::cerr << "Output:\n" << palindrome << "\n";
+
+    std::cerr << "List after testing:\n";
+    for(ListNode *l=head; l; l=l->next) {
+        std::cerr << l->val << "\n";
+    }
+
+    return !palindrome;
 }
